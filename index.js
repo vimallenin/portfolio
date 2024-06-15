1a32f925869eaa39ad7c4d46fa1a23d7ccdb1acd
vimallenin <133043066+vimallenin@users.noreply.github.com>
2024-06-15 20:51:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.getElementById('burger');
+    const navLinks = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('show');
+    });
+});
 
