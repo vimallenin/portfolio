85f4147c2f4a5298a3996be83f2b57529c2edf23
vimallenin <133043066+vimallenin@users.noreply.github.com>
2024-06-16 13:05:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const burger = document.getElementById('burger');
-    const navLinks = document.querySelector('.nav-links');
 
-    burger.addEventListener('click', () => {
+const burger = document.getElementById('burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
         navLinks.classList.toggle('active');
-    });
+});
 
     // Close the menu when a link is clicked
-    navLinks.addEventListener('click', (event) => {
-        if (event.target.tagName === 'A') {
+navLinks.addEventListener('click', () => {
             navLinks.classList.remove('active');
-        }
-    });
 });
