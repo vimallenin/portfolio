d6b8e1fbea05bb88806fae556ba572786c41de47
vimallenin <133043066+vimallenin@users.noreply.github.com>
2024-06-16 11:56:13 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.getElementById('burger');
+    const navLinks = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('show');
+    });
+});
